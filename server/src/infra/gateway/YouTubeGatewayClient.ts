05586e575a36a0a8cbecf32664d8115094246fb4
Marcos Kuchak <mkuchak@hotmail.com>
2024-01-30 17:32:37 -0300

    fix: fix the gateway that searches for video transcripts

--- server/src/infra/gateway/YouTubeGatewayClient.ts
+++ server/src/infra/gateway/YouTubeGatewayClient.ts
@@ -1,50 +1,20 @@
 import { VideoData, YouTubeGateway } from "@/contract/gateway/YouTubeGateway";
-import Api from "youtube-browser-api";
-
-interface VideoBrowserData {
-  videoDetails: {
-    title: string;
-    shortDescription: string;
-  };
-  thumbnails: Array<{
-    url: string;
-    height: number;
-    width: number;
-  } | null>;
-  formats: Array<{
-    url: string;
-  }>;
-}
+import { fetchTranscript } from "youtube-fetch-transcript";
 
 export class YouTubeGatewayClient implements YouTubeGateway {
   static async getVideo(videoId: string): Promise<VideoData> {
-    const { videoId: dialogs } = await Api.transcript({ videoId });
-    const info = (await Api.query({
-      id: videoId,
-      schema: {
-        playerResponse: {
-          videoDetails: {
-            title: true,
-            thumbnail: {
-              thumbnails: {
-                4: {
-                  url: true,
-                  height: true,
-                  width: true,
-                },
-              },
-            },
-          },
-        },
-      },
-      paths: "playerResponse.streamingData.formats.0.url",
-    })) as unknown as VideoBrowserData;
+    const videoTranscript = await fetchTranscript(videoId);
+    const videoData = (await (
+      await fetch(
+        `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`
+      )
+    ).json()) as any;
 
     return {
-      title: info?.videoDetails?.title,
-      thumbnail: info?.thumbnails?.[4]?.url,
-      url: info?.formats?.[0]?.url,
-      dialogs: dialogs || [],
+      title: videoData.title,
+      thumbnail: videoData.thumbnail_url.replace("hqdefault", "maxresdefault"),
+      url: `https://www.youtube.com/watch?v=${videoId}`,
+      dialogs: videoTranscript,
     };
   }
 }
